2cb2baefc9f3e9ffd4a35e534e23a260af7f6dea
alebaffa <alessandro.baffa@gmail.com>
2015-10-20 23:41:29 +0200

    add raindrops and fixed comment formatting in leap

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,7 +1,9 @@
 package leap
 
-//TestVersion is the testing version number
+// TestVersion is the testing version number.
 const TestVersion = 1
 
-//IsLeapYear checks if the input year is leap or not
-func IsLeapYear(year int) bool { return year%4 == 0 && (year%100 != 0 || year%400 == 0) }
+// IsLeapYear checks if the input year is leap or not.
+func IsLeapYear(year int) bool {
+	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
+}
--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,27 +1,34 @@
 package raindrops
 
-import "math"
+import (
+	"strconv"
+	"strings"
+)
 
-//primeFactors returns a string depending on the prime factors of the given input
 func primeFactors(num int) string {
-	//check if the number is divisible per 2
-	for num%2 == 0 {
-		num /= 2
+	//create a slice of string.
+	resultString := []string{}
+	// append the correct string depending on the prime factor.
+	if num%3 == 0 {
+		resultString = append(resultString, "Pling")
 	}
-
-	for index := 3; index <= int(math.Sqrt(float64(num))); index += 2 {
-		for num%index == 0 {
-			n /= index
-		}
+	if num%5 == 0 {
+		resultString = append(resultString, "Plang")
 	}
-
-	if num > 2 {
-
+	if num%7 == 0 {
+		resultString = append(resultString, "Plong")
 	}
-	return ""
+	// In case the string so far is empty,
+	// it means that prime factors are not 3, 5 nor 7.
+	// So, print the original number.
+	if len(resultString) == 0 {
+		resultString = append(resultString, strconv.Itoa(num))
+	}
+	return strings.Join(resultString, "")
 }
 
-//Convert converts a given int number in to a string depending on its prime factors
+// Convert converts a given int number in to a string
+// depending on its prime factors.
 func Convert(num int) string {
 	return primeFactors(num)
 }
