617bb85e4b466d36bb8e5b34a489e2434fc033c3
alebaffa <alessandro.baffa@gmail.com>
2015-10-28 07:44:08 +0100

    add new exercism in go - triangle

---
+++ go/triangle/triangle.go
@@ -0,0 +1,48 @@
+package triangle
+
+import "math"
+
+type Kind int
+
+const (
+	Equ Kind = iota
+	Iso
+	Sca
+	NaT
+)
+
+func (k Kind) String() string {
+	switch k {
+	case Equ:
+		return "Equ"
+	case Iso:
+		return "Iso"
+	case Sca:
+		return "Sca"
+	case NaT:
+		return "NaT"
+	default:
+		panic("should not occur")
+	}
+}
+
+func KindFromSides(a, b, c float64) Kind {
+	// I create an array with the sides so that I can easily loop on them without using multiple if
+	triangle := []float64{a, b, c}
+	for _, side := range triangle {
+		if side < 0 || math.IsNaN(side) {
+			return NaT
+		}
+	}
+
+	switch {
+	case a+b <= c || a+c <= b || b+c <= a:
+		return NaT
+	case a == b && b == c:
+		return Equ
+	case a == b || a == c || b == c:
+		return Iso
+	default:
+		return Sca
+	}
+}
