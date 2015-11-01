6961b3ade3e24776316c2fdaa0a0c036b4671daf
alebaffa <alessandro.baffa@gmail.com>
2015-11-01 19:29:38 +0100

    3rd iteration, more cool, for secret_handshake

--- go/secret-handshake/secret_handshake.go
+++ go/secret-handshake/secret_handshake.go
@@ -4,26 +4,26 @@ package secret
 func Handshake(code int) []string {
 
 	var result []string
+	events := []string{"wink", "double blink", "close your eyes", "jump"}
 
 	if code < 0 {
 		return result
 	}
-
-	if code&(1<<0) > 0 {
-		result = append(result, "wink")
-	}
-	if code&(1<<1) > 0 {
-		result = append(result, "double blink")
-	}
-	if code&(1<<2) > 0 {
-		result = append(result, "close your eyes")
-	}
-	if code&(1<<3) > 0 {
-		result = append(result, "jump")
+	/*Here I can directly manipulates the bits in the input number.
+		Ex.: 19 = 10011. I can shift the number 1 to the number of positions I want to check in the input number
+		(1<<x, x being the num of positions. It will give 2^x value corresponding to the position-x, in case there is 1 in that position).
+	  Then, I can AND it with the input number. If it is greater than zero, it means that the bit is set in that position.
+	*/
+	for index, _ := range events {
+		i := uint(index)
+		if code&(1<<i) > 0 {
+			result = append(result, events[index])
+		}
 	}
 	if code&(1<<4) > 0 {
 		result = reverse(result)
 	}
+
 	return result
 }
 
