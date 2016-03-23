6852cb57cabee56073e754579ca66339dde9af6b
Alessandro Baffa <alessandro.baffa@gmail.com>
2016-03-23 07:19:22 +0100

    add house exercise - TODO

---
+++ go/house/house.go
@@ -0,0 +1,13 @@
+package house
+
+func Embed(l, p string) string {
+	return l + " " + p
+}
+
+func Verse(s string, ri []string, p string) string {
+	return ""
+}
+
+func Song() string {
+	return ""
+}
