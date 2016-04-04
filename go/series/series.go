8e7ab21db2833c010f4d5369d5888f8bfff97d65
Alessandro Baffa <alessandro.baffa@gmail.com>
2016-04-04 07:51:35 +0200

    add series solution in Go

---
+++ go/series/series.go
@@ -0,0 +1,17 @@
+package slice
+
+// All returns the contiguous substrings of given length in the given number
+func All(length int, number string) []string {
+	var substrings []string
+	for index := 0; index < len(number); index++ {
+		if index+length <= len(number) {
+			substrings = append(substrings, number[index:index+length])
+		}
+	}
+	return substrings
+}
+
+// Frist returns the first length elements in the given number
+func Frist(length int, number string) string {
+	return number[:length]
+}
