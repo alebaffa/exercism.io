5ad976556d52a0c15f4d1e456109e595d7631089
Alessandro Baffa <alessandro.baffa@gmail.com>
2016-04-05 00:17:53 +0200

    queen-attack tests pass. TODO: refactoring

--- go/queen-attack/queen_attack.go
+++ go/queen-attack/queen_attack.go
@@ -4,12 +4,15 @@ import "errors"
 
 func CanQueenAttack(whiteQueen, blackQueen string) (bool, error) {
 
-	if !isInBoard(whiteQueen) || !isInBoard(blackQueen) || whiteQueen == blackQueen {
+	if !isInBoard(whiteQueen) || !isInBoard(blackQueen) {
 		return false, errors.New("off the board")
+	}
+	if whiteQueen == blackQueen {
+		return false, errors.New("queens in the same position")
 	}
 
 	distanceInRow, distanceInCol := calculateDistances(whiteQueen, blackQueen)
-	return (distanceInRow == 0 || distanceInCol == 0 || distanceInRow == distanceInCol), nil
+	return (distanceInRow == 0 || distanceInCol == 0 || distanceInRow == distanceInCol || distanceInRow == -distanceInCol), nil
 }
 
 func isInBoard(queen string) bool {
@@ -22,5 +25,7 @@ func isInBoard(queen string) bool {
 }
 
 func calculateDistances(whiteQueen, blackQueen string) (int, int) {
-	return int(whiteQueen[0]) - int(blackQueen[0]), int(whiteQueen[1]) - int(blackQueen[1])
+	rowDistance := int(whiteQueen[0]) - int(blackQueen[0])
+	colDistance := int(whiteQueen[1]) - int(blackQueen[1])
+	return rowDistance, colDistance
 }
