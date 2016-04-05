55950c047c5ac2d7b271134f6e9d647ff240ccf8
Alessandro Baffa <alessandro.baffa@gmail.com>
2016-04-05 07:35:29 +0200

    refactored queen_attack in go

--- go/queen-attack/queen_attack.go
+++ go/queen-attack/queen_attack.go
@@ -2,30 +2,44 @@ package queenattack
 
 import "errors"
 
+// CanQueenAttack calculates the positions of the two queens in input and defines if the first queen can attack the second one.
 func CanQueenAttack(whiteQueen, blackQueen string) (bool, error) {
 
-	if !isInBoard(whiteQueen) || !isInBoard(blackQueen) {
-		return false, errors.New("off the board")
-	}
-	if whiteQueen == blackQueen {
-		return false, errors.New("queens in the same position")
+	if !isValid(whiteQueen) || !isValid(blackQueen) {
+		return false, errors.New("coordinates are not valid")
 	}
 
-	distanceInRow, distanceInCol := calculateDistances(whiteQueen, blackQueen)
-	return (distanceInRow == 0 || distanceInCol == 0 || distanceInRow == distanceInCol || distanceInRow == -distanceInCol), nil
+	if !isInBoard(whiteQueen) || !isInBoard(blackQueen) || whiteQueen == blackQueen {
+		return false, errors.New("off the board")
+	}
+
+	rowDist, colDist := distances(whiteQueen, blackQueen)
+	return (onSameRowOrColumn(rowDist, colDist) || onSameDiagonal(rowDist, colDist)), nil
 }
 
 func isInBoard(queen string) bool {
-	if len(queen) != 2 {
-		return false
-	}
 	rowInBoard := ('a' <= queen[0] && queen[0] <= 'h')
 	colInBoard := ('1' <= queen[1] && queen[1] <= '8')
 	return rowInBoard && colInBoard
 }
 
-func calculateDistances(whiteQueen, blackQueen string) (int, int) {
+func isValid(queen string) bool {
+	if len(queen) != 2 {
+		return false
+	}
+	return true
+}
+
+func distances(whiteQueen, blackQueen string) (int, int) {
 	rowDistance := int(whiteQueen[0]) - int(blackQueen[0])
 	colDistance := int(whiteQueen[1]) - int(blackQueen[1])
 	return rowDistance, colDistance
+}
+
+func onSameRowOrColumn(rowDist, colDist int) bool {
+	return rowDist == 0 || colDist == 0
+}
+
+func onSameDiagonal(rowDist, colDist int) bool {
+	return rowDist == colDist || rowDist == -colDist
 }
