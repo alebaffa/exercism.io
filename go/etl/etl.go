3e9799c13fac72132f178f41a1529f1651d7a4c0
Alessandro Baffa <alessandro.baffa@gmail.com>
2016-05-10 21:45:46 +0200

    solution etl in Go - iteration 1

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -1,7 +1,14 @@
 package etl
 
-func Transform(map[int][]string) map[string]int {
+import "strings"
+
+func Transform(oldMap map[int][]string) map[string]int {
 	newMap := map[string]int{}
 
+	for num, letters := range oldMap {
+		for _, letter := range letters {
+			newMap[strings.ToLower(letter)] = num
+		}
+	}
 	return newMap
 }
