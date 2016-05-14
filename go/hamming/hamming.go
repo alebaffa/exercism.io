ae657d763087b52fa94fe63a13ab6473a8c8b94d
Alessandro Baffa <alessandro.baffa@gmail.com>
2016-05-14 16:20:18 +0200

    4th iteration hamming.go

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -4,16 +4,22 @@ import "errors"
 
 const testVersion = 4
 
+var errSize = errors.New("The two DNA have different size!")
+
 /*Distance compares two DNA strands (DNAa, b) and returns how many nucleotides are different in the two. */
 func Distance(a, b string) (int, error) {
 	if len(a) != len(b) {
-		return -1, errors.New("Error")
+		return -1, errSize
 	}
 
 	return countDistance(a, b)
 }
 
 func countDistance(a, b string) (int, error) {
+	if a == b {
+		return 0, nil
+	}
+
 	distance := 0
 	for index, value := range a {
 		nucleotidA := string(value)
