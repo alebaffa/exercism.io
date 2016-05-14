c85e79073df1023bf49232aa0fea9a0c88ffa370
Alessandro Baffa <alessandro.baffa@gmail.com>
2016-05-14 16:13:03 +0200

    3rd iteration - hamming Golang

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -5,15 +5,23 @@ import "errors"
 const testVersion = 4
 
 /*Distance compares two DNA strands (DNAa, b) and returns how many nucleotides are different in the two. */
-func Distance(DNAa, DNAb string) (int, error) {
-	if len(DNAa) != len(DNAb) {
+func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
 		return -1, errors.New("Error")
 	}
-	count := 0
-	for indexA, nucleotidA := range DNAa {
-		if string(nucleotidA) != string(DNAb[indexA]) {
-			count++
+
+	return countDistance(a, b)
+}
+
+func countDistance(a, b string) (int, error) {
+	distance := 0
+	for index, value := range a {
+		nucleotidA := string(value)
+		nucleotidB := string(b[index])
+
+		if nucleotidA != nucleotidB {
+			distance++
 		}
 	}
-	return count, nil
+	return distance, nil
 }
