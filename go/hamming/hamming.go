a759c6fdd9c9e78e16c3117a337a1dd40082b3e6
Alessandro Baffa <alessandro.baffa@gmail.com>
2016-05-15 12:05:04 +0200

    5th iteration of go/hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,12 +1,16 @@
 package hamming
 
-import "errors"
+import (
+	"errors"
+	"strings"
+)
 
 const testVersion = 4
 
 var errSize = errors.New("The two DNA have different size!")
 
-/*Distance compares two DNA strands (DNAa, b) and returns how many nucleotides are different in the two. */
+// Distance compares two DNA strands (DNAa, b) and returns
+// how many nucleotides are different in the two. */
 func Distance(a, b string) (int, error) {
 	if len(a) != len(b) {
 		return -1, errSize
@@ -16,18 +20,25 @@ func Distance(a, b string) (int, error) {
 }
 
 func countDistance(a, b string) (int, error) {
+	// this check works also with strings that have different
+	// lenght of bytes (but still the same num of 'characters' - e.g.:'ä' and 'a')
 	if a == b {
 		return 0, nil
 	}
 
 	distance := 0
-	for index, value := range a {
-		nucleotidA := string(value)
-		nucleotidB := string(b[index])
+	readerA, readerB := getReader(a, b)
 
-		if nucleotidA != nucleotidB {
+	for readerA.Len() != 0 {
+		charA, _, _ := readerA.ReadRune()
+		charB, _, _ := readerB.ReadRune()
+		if charA != charB {
 			distance++
 		}
 	}
 	return distance, nil
+}
+
+func getReader(a, b string) (readrA, readrB *strings.Reader) {
+	return strings.NewReader(a), strings.NewReader(b)
 }
