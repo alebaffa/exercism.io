4d1659d3e3f5ddcd7a250bd7b5d082840de0d236
Alessandro BAFFA <alessandro.baffa@amadeus.com>
2016-11-04 16:44:15 +0100

    polish regex

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -8,12 +8,15 @@ const testVersion = 2
 type Frequency map[string]int
 
 func WordCount(phrase string) Frequency {
-	regex := regexp.MustCompile("[A-Za-z]+|[1-9]+")
-	words := regex.FindAllString(phrase, -1)
+	words := getWords(phrase)
 
 	result := make(Frequency)
 	for _, word := range words {
 		result[strings.ToLower(word)]++
 	}
 	return result
+}
+
+func getWords(phrase string) []string {
+	return regexp.MustCompile("[A-Za-z1-9]+").FindAllString(phrase, -1)
 }
