957c1a3049a4a15355ebed6a1fb75e14891a4147
Alessandro Baffa <alessandro.baffa@gmail.com>
2018-01-29 23:16:40 +0900

    iteration 3 more elegant

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -3,43 +3,31 @@ package space
 type Planet string
 
 const (
-	secondsInDay     float64 = 24 * 60 * 60
-	earthOrbitDays   float64 = 365.25
-	earthSecondsYear float64 = 31557600
-	mercuryOrbitDays float64 = 0.2408467
-	venusOrbitDays   float64 = 0.61519726
-	marsOrbitDays    float64 = 1.8808158
-	jupiterOrbitDays float64 = 11.862615
-	saturnOrbitDays  float64 = 29.447498
-	uranusOrbitDays  float64 = 84.016846
-	neptuneOrbitDays float64 = 164.79132
+	Earth   Planet = "Earth"
+	Mercury        = "Mercury"
+	Venus          = "Venus"
+	Mars           = "Mars"
+	Jupiter        = "Jupiter"
+	Saturn         = "Saturn"
+	Uranus         = "Uranus"
+	Neptune        = "Neptune"
 )
 
-func Age(seconds float64, planet Planet) float64 {
-	switch planet {
-	case "Earth":
-		return seconds / earthSecondsYear
-	case "Mercury":
-		return seconds / calculate(mercuryOrbitDays)
-	case "Venus":
-		return seconds / calculate(venusOrbitDays)
-	case "Mars":
-		return seconds / calculate(marsOrbitDays)
-	case "Jupiter":
-		return seconds / calculate(jupiterOrbitDays)
-	case "Saturn":
-		return seconds / calculate(saturnOrbitDays)
-	case "Uranus":
-		return seconds / calculate(uranusOrbitDays)
-	case "Neptune":
-		return seconds / calculate(neptuneOrbitDays)
-	}
-	return 0.0
+const earthAgeSecond = 31557600
 
+var planetPeriods = map[Planet]float64{
+	Earth:   1,
+	Mercury: 0.2408467,
+	Venus:   0.61519726,
+	Mars:    1.8808158,
+	Jupiter: 11.862615,
+	Saturn:  29.447498,
+	Uranus:  84.016846,
+	Neptune: 164.79132,
 }
 
-func calculate(planetOrbitDays float64) float64 {
-	planetOrbitPeriod := earthOrbitDays * planetOrbitDays
-	orbitSeconds := planetOrbitPeriod * secondsInDay
-	return orbitSeconds
+func Age(second float64, planet Planet) float64 {
+	period := planetPeriods[planet] * earthAgeSecond
+
+	return second / period
 }
